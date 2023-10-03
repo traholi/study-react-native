0df64bbf535c6b15f509c65a2c6d9f5dbc20bda9
oura-hideyoshi <tomatommy.bs@gmail.com>
2023-10-03 20:42:53 +0900

    Remove Firebase API and config files

--- src/constnts/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyBeUUSVS1AOj2-5IqAT7y0YPmXVB8swk3U',
-  authDomain: 'travelholic-15ae1.firebaseapp.com',
-  projectId: 'travelholic-15ae1',
-  storageBucket: 'travelholic-15ae1.appspot.com',
-  messagingSenderId: '617770204509',
-  appId: '1:617770204509:web:f80665d142ec6189e07cb4',
-  measurementId: 'G-R7WMSK2CNH',
-};
